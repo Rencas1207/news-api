ab6915065e3bc3e038ab5cb21f1d564e12d4ec97
Rencas <renzo.c@ohecommerce.agency>
2023-04-08 17:28:49 -0500

    news-api request api news

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { Container, Grid, Typography } from '@mui/material';
+import Formulario from './components/Formulario';
+import { NewsProvider } from './context/NewsProvider';
 
 function App() {
 
   return (
-    <h1>Hola Mundo!</h1>
+    <NewsProvider>
+      <Container>
+        <header>
+          <Typography align='center' marginY={5} component="h1" variant='h3'>
+            Buscador de noticias
+          </Typography>
+        </header>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignContent="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+      </Container>
+    </NewsProvider>
   )
 }
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,56 @@
+import {
+   FormControl,
+   InputLabel,
+   Select,
+   MenuItem,
+   Button,
+   Box
+} from '@mui/material';
+import useNews from '../hooks/useNews';
+
+const CATEGORIAS = [
+   { value: 'general', label: 'General' },
+   { value: 'business', label: 'Negocios' },
+   { value: 'entertainment', label: 'Entretenimiento' },
+   { value: 'health', label: 'Salud' },
+   { value: 'science', label: 'Ciencia' },
+   { value: 'sports', label: 'Deportes' },
+   { value: 'technology', label: 'Tecnología' },
+]
+
+const Formulario = () => {
+
+   const { category, handleChangeCategory } = useNews();
+
+   return (
+      <form>
+         <FormControl fullWidth>
+            <InputLabel>Categoria</InputLabel>
+            <Select
+               label="Categoria"
+               onChange={handleChangeCategory}
+               value={category}
+            >
+               {
+                  CATEGORIAS.map(category => (
+                     <MenuItem key={category.value} value={category.value}>
+                        {category.label}
+                     </MenuItem>
+                  ))
+               }
+            </Select>
+            <Box sx={{ marginTop: 2 }}>
+               <Button
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+               >
+                  Buscar Noticias
+               </Button>
+            </Box>
+         </FormControl>
+      </form>
+   )
+}
+
+export default Formulario
